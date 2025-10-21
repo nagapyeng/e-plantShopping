ee46a197692d8a6ac3cb62506879f2eb377e59df
Naga Allati <nagapyeng@gmail.com>
2025-10-21 11:38:11 -0400

    Prepare for deployment

--- src/store.js
+++ src/store.js
@@ -1,11 +1,18 @@
 // src/store.js
+
+// 1. Import configureStore from Redux Toolkit
 import { configureStore } from '@reduxjs/toolkit';
+
+// 2. Import your reducer(s)
 import cartReducer from './CartSlice';
 
+// 3. Configure the store with your slice reducers
 const store = configureStore({
   reducer: {
-    cart: cartReducer
-  }
+    // cart slice managed by cartReducer
+    cart: cartReducer,
+  },
 });
 
+// 4. Export the store to use in <Provider> and throughout your app
 export default store;
